3be9f3748b8686bfcfa09cd40efe6db3dfaf6ce6
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-16 17:54:21 +0800

    wrong items

--- js/application_management.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$(".application-management .row .application").click(function() {
-		$description = $(this).parent().find(".description");
-		
-		if ( $description.css("display") == 'none' ) $description.slideDown("slow");
-		else $description.slideUp("slow");
-	});
-});
--- js/bottom_latest.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$(".bottom_latest .photo").mouseenter(function() {
-		$(".bottom_latest .photo .subject").hide();
-		$(this).children('.subject').show();
-	});
-	$(".bottom_latest .photo").mouseleave(function() {
-		$(this).children('.subject').hide();
-	});
-});
